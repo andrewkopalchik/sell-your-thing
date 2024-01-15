21856ce262783c5f96c43737a3224df998bf21e7
m1shan4ik <101133672+m1shan4ik@users.noreply.github.com>
2024-01-15 15:50:06 +0200

    .

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
+import { FaShoppingCart } from "react-icons/fa"
+import { useState } from 'react'
 
 export const Navbar = () => {
+	let [cartOpen, setCartOpen] = useState(false)
   return (
 	<nav className='nav'>
 		<Link to='/' className ='site-title'>
 			Sale your thing
 		</Link>
 		<ul>
+		<FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+		{cartOpen && (
+			<div className='shop-cart'>
+				
+			</div>
+		)}
 			<li className='active'>			
 				<Link to='/pricing'>Pricing page</Link>
 			</li>
